d99360b4a2774dc1c8334416bf021361e9800d33
Sofia <balochsofia@gmail.com>
2019-06-17 23:03:19 +0500

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_162035) do
+ActiveRecord::Schema.define(version: 2019_06_17_174532) do
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
